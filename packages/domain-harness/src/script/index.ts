07cbb14def7a6640f00da4135bc358c856026bf0
kaicreator-mm <bba287191@gmail.com>
2026-09-17 09:27:17 +0630

    feat(t07): export internal Script executor module

---
+++ packages/domain-harness/src/script/index.ts
@@ -0,0 +1,8 @@
+export {
+  ScriptExecutor,
+  ScriptExecutorError,
+} from './script-executor.js';
+export type {
+  ScriptExecutionOptions,
+  ScriptExecutorErrorCode,
+} from './script-executor.js';
